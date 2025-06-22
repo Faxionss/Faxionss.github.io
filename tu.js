8a6f9187b76a7368f57a4f837eb8ecd254db6372
Faxionss <facundostrappolini1@gmail.com>
2025-06-22 01:41:49 -0300

    Add files via upload

---
+++ tu.js
@@ -0,0 +1,88 @@
+
+document.querySelector("#btnNombre").addEventListener("click", IngresarNombre)
+
+document.querySelector("#btnContinuar1").addEventListener("click", continuarProximo) 
+
+document.querySelector("#btnContinuar2").addEventListener("click", continuarProximo2) 
+document.querySelector("#btnContinuarApellidoTrue").addEventListener("click", ContinuarApellido) 
+document.querySelector("#btnPerroNombre").addEventListener("click", IngresarPerro) 
+
+
+
+
+
+function IngresarPerro(){
+    let perro =  document.querySelector("#txtPerroNombre").value.toLowerCase()
+
+    if( perro == "oplur"){
+          document.querySelector("#pantallaAravenaTrue").style.display = "none"
+          document.querySelector("#carta").style.display = "block"
+
+    }else{
+          document.querySelector("#pantallaAravenaTrue").style.display = "none"
+          document.querySelector("#error").style.display = "block"
+
+    }
+}
+
+function ContinuarApellido(){
+     let select = document.querySelector("#slcApellido").value
+
+     if(select == "Si"){
+         document.querySelector("#pantallaApellidoTrue").style.display = "none"
+             document.querySelector("#pantallaAravenaTrue").style.display = "block"
+                }else{
+         document.querySelector("#error").style.display = "block"
+          document.querySelector("#pantallaApellidoTrue").style.display = "none"
+
+    }
+
+     }
+     
+function continuarProximo(){
+    document.querySelector("#pantallaBienvenidoTrue").style.display = "none"
+     document.querySelector("#pantallaApellido").style.display = "block"
+      
+}
+
+function IngresarNombre(){
+    let nombre = document.querySelector("#txtNombre").value.toLowerCase()
+
+    if(nombre == "alexander"){
+             document.querySelector("#pantallaBienvenido").style.display = "none"
+          document.querySelector("#pantallaBienvenidoTrue").style.display = "block"
+         
+    }else{
+         document.querySelector("#pantallaBienvenido").style.display = "none"
+        document.querySelector("#pantallaBienvenidoFalse").style.display = "block"
+    }
+}
+
+function continuarProximo2(){
+    let inicial =  document.querySelector("#txtApellido").value.toLowerCase()
+
+    if(inicial == "a"){
+ document.querySelector("#pantallaApellido").style.display = "none"
+    document.querySelector("#pantallaApellidoTrue").style.display = "block"
+    }else{
+         document.querySelector("#error").style.display = "block"
+          document.querySelector("#pantallaApellido").style.display = "none"
+
+    }
+}
+
+
+ocultarTodo()
+
+function ocultarTodo(){
+    document.querySelector("#pantallaBienvenido").style.display = "block"
+    document.querySelector("#pantallaBienvenidoTrue").style.display = "none"
+    document.querySelector("#pantallaBienvenidoFalse").style.display = "none"
+    document.querySelector("#pantallaApellido").style.display = "none"
+    document.querySelector("#error").style.display = "none"
+    document.querySelector("#pantallaApellidoTrue").style.display = "none"
+    document.querySelector("#pantallaAravenaTrue").style.display = "none"
+    document.querySelector("#carta").style.display = "none"
+
+}
+
